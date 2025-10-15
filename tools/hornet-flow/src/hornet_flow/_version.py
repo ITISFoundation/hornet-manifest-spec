128bbae76d070a9774a673ae0af299436aab29a0
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-10-15 13:25:13 +0200

    feat: update package version to 0.3.0 and enhance versioning documentation

--- tools/hornet-flow/src/hornet_flow/_version.py
+++ tools/hornet-flow/src/hornet_flow/_version.py
@@ -1,4 +1,12 @@
-"""Version information for hornet-flow package."""
+"""Version information for hornet-flow package
+
+## Versioning Scheme
+    Uses a custom versioning scheme in which 
+        - the minor version number is bumped for breaking changes, and 
+        - the patch version number is bumped for bug fixes, enhancements, and other non-breaking changes.
+
+    Once v1.0.0 is released, the versioning scheme will adhere to [Semantic Versioning](https://semver.org/).
+"""
 
 import importlib.metadata
 from typing import Final
